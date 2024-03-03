e1fddb0102690a7778e883326872f345fc004fff
Muthuramanathan <63590927+muthuramanathanm@users.noreply.github.com>
2024-03-03 15:08:50 +0800

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': 'Hello from Lambda, This is CICD pipeline example!'
+        'body': 'Hello from Lambda, This is CI-CD pipeline example, added!'
     }
